3e5b9f52692cbb4fde0fe7d814fb511422d2f49c
felipy <felipy81br@gmail.com>
2026-05-12 10:26:01 -0300

    feat: Criando mapeador para entidade Categoria e o model do sequelize

---
+++ src/category/infra/db/sequelize/category-model-mapper.ts
@@ -0,0 +1,42 @@
+import { Uuid } from "../../../../shared/domain/value-objects/uuid.vo";
+import { Category } from "../../../domain/category.entity";
+import { CategoryModel } from "./category.model";
+
+export class CategoryModelMapper {
+  /*
+    Pergunta:
+    Tem 3 formas de usar o model no sequelize, dê um exemplo de cada.
+
+    Resposta:
+
+    Active Record (Instância):
+    const instance = CategoryModel.build({...}); await instance.save();
+
+    Método Estático:
+    await CategoryModel.create({...});
+
+    Consulta de Dados:
+    const result = await CategoryModel.findByPk(id);
+  */
+	static toModel(entity: Category): CategoryModel {
+    return CategoryModel.build({
+      category_id: entity.category_id.id,
+      name: entity.name,
+      description: entity.description,
+      is_active: entity.is_active,
+      created_at: entity.created_at,
+    });
+  }
+
+  static toEntity(model: CategoryModel): Category {
+    const entity = new Category({
+      category_id: new Uuid(model.category_id),
+      name: model.name,
+      description: model.description,
+      is_active: model.is_active,
+      created_at: model.created_at,
+    });
+    Category.validate(entity);
+    return entity;
+  }
+}
