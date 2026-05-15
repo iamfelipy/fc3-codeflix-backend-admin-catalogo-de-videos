151ce6f0646b90da20097d54cc3fe7c35b57b5d2
felipy <felipy81br@gmail.com>
2026-05-15 19:09:04 +0000

    feat: Testes do caso de uso de listagem de categorias

---
+++ src/category/application/use-cases/__tests__/integration/list-categories.use-case.int-spec.ts
@@ -0,0 +1,100 @@
+import { ListCategoriesUseCase } from '../../list-categories.use-case';
+import { setupSequelize } from '../../../../../shared/infra/testing/helpers';
+import { Category } from '../../../../domain/category.entity';
+import { CategorySequelizeRepository } from '../../../../infra/db/sequelize/category-sequelize.repository';
+import { CategoryModel } from '../../../../infra/db/sequelize/category.model';
+import { CategoryOutputMapper } from '../../common/category-output';
+
+describe('ListCategoriesUseCase Integration Tests', () => {
+  let useCase: ListCategoriesUseCase;
+  let repository: CategorySequelizeRepository;
+
+  setupSequelize({ models: [CategoryModel] });
+
+  beforeEach(() => {
+    repository = new CategorySequelizeRepository(CategoryModel);
+    useCase = new ListCategoriesUseCase(repository);
+  });
+
+  it('should return output sorted by created_at when input param is empty', async () => {
+    const categories = Category.fake()
+      .theCategories(2)
+      .withCreatedAt((i) => new Date(new Date().getTime() + 1000 + i))
+      .build();
+
+    await repository.bulkInsert(categories);
+    const output = await useCase.execute({});
+    expect(output).toEqual({
+      items: [...categories].reverse().map(CategoryOutputMapper.toOutput),
+      total: 2,
+      current_page: 1,
+      per_page: 15,
+      last_page: 1,
+    });
+  });
+
+  it('should returns output using pagination, sort and filter', async () => {
+    /*
+      No Sequelize (e geralmente em SQL), a ordenação padrão (ORDER BY) diferencia maiúsculas de minúsculas, então "A" (maiúsculo) vem antes de "a" (minúsculo) com collation padrão (UTF8_bin ou similar). Mas o comportamento exato depende do collation/tabela do banco. Se for um collation case-insensitive (utf8_general_ci), "A" e "a" são equivalentes na ordenação.
+    */
+    const categories = [
+      new Category({ name: 'a' }),
+      new Category({
+        name: 'AAA',
+      }),
+      new Category({
+        name: 'AaA',
+      }),
+      new Category({
+        name: 'b',
+      }),
+      new Category({
+        name: 'c',
+      }),
+    ];
+    await repository.bulkInsert(categories);
+
+    let output = await useCase.execute({
+      page: 1,
+      per_page: 2,
+      sort: 'name',
+      filter: 'a',
+    });
+    expect(output).toEqual({
+      items: [categories[1], categories[2]].map(CategoryOutputMapper.toOutput),
+      total: 3,
+      current_page: 1,
+      per_page: 2,
+      last_page: 2,
+    });
+
+    output = await useCase.execute({
+      page: 2,
+      per_page: 2,
+      sort: 'name',
+      filter: 'a',
+    });
+    expect(output).toEqual({
+      items: [categories[0]].map(CategoryOutputMapper.toOutput),
+      total: 3,
+      current_page: 2,
+      per_page: 2,
+      last_page: 2,
+    });
+
+    output = await useCase.execute({
+      page: 1,
+      per_page: 2,
+      sort: 'name',
+      sort_dir: 'desc',
+      filter: 'a',
+    });
+    expect(output).toEqual({
+      items: [categories[0], categories[2]].map(CategoryOutputMapper.toOutput),
+      total: 3,
+      current_page: 1,
+      per_page: 2,
+      last_page: 2,
+    });
+  });
+});
