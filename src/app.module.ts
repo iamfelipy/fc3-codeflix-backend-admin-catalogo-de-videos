71ef1f26cfc4283f950bf2353388df62ad049438
felipy <felipy81br@gmail.com>
2026-05-20 11:46:23 +0000

    feat: Integração do Nest.js com Sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
+import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [CategoriesModule],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'sqlite' as any,
+      host: ':memory:',
+      logging: false,
+      models: [CategoryModel],
+    }),
+    CategoriesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
