10631a1235a43196695659864e94ccf9049c1f63
felipy <felipy81br@gmail.com>
2026-05-24 11:36:21 +0000

    feat: Organizando pastas dos módulos do Nest.js

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CategoriesModule } from './categories/categories.module';
-import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from './config/config.module';
+import { CategoriesModule } from './nest-modules/categories-module/categories.module';
+import { DatabaseModule } from './nest-modules/database-module/database.module';
+import { ConfigModule } from './nest-modules/config-module/config.module';
 
 @Module({
   imports: [
@@ -11,7 +9,5 @@ import { ConfigModule } from './config/config.module';
     DatabaseModule,
     CategoriesModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
