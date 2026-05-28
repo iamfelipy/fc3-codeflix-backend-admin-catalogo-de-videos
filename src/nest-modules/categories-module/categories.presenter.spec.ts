dc967a96828089d790557f71e0a5090e9e029ad2
felipy <felipy81br@gmail.com>
2026-05-28 11:25:25 -0300

    feat: Testes com class-transformer e collection e pagination presenter

---
+++ src/nest-modules/categories-module/categories.presenter.spec.ts
@@ -0,0 +1,39 @@
+import { instanceToPlain } from "class-transformer";
+import { CategoryPresenter } from "./categories.presenter";
+
+describe("Category Presenter Unit Tests", () => {
+  describe('constructor', () => {
+    it('should set values', () => {
+      const presenter = new CategoryPresenter({
+        id: 'XXX-XXX',
+        name: 'roupa',
+        description: 'roupas da loja',
+        is_active: true,
+        created_at: new Date()
+      });
+
+      expect(presenter.id).toBe('XXX-XXX');
+      expect(presenter.name).toBe('melissa');
+      expect(presenter.description).toBe('ana');
+      expect(presenter.created_at).toBeInstanceOf(Date);
+ 
+    });
+  });
+
+  it('should presenter data', () => {
+    const presenter = new CategoryPresenter({
+      id: 'XXX-XXX',
+      name: 'roupa',
+      description: 'roupas da loja',
+      is_active: true,
+      created_at: new Date()
+    });
+
+    expect(instanceToPlain(presenter)).toStrictEqual({
+      id: 'XXX-XXX',
+      name: 'roupa',
+      description: 'roupas da loja',
+      created_at: presenter.created_at.toISOString(),
+    });
+  });
+})
