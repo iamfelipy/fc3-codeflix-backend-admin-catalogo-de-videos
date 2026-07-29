e5f77e03d6eb356e10af4a417b13767b91207449
felipy <felipy81br@gmail.com>
2026-07-29 15:17:00 -0300

    feat: Disparando domain events

--- src/core/shared/domain/aggregate-root.ts
+++ src/core/shared/domain/aggregate-root.ts
@@ -1,9 +1,18 @@
+import EventEmitter2 from 'eventemitter2';
 import { Entity } from './entity';
 import { IDomainEvent } from './events/domain-event.interface';
 
 export abstract class AggregateRoot extends Entity {
-  //vai disparar somente o evento dentro do próprio aggregate
-  applyEvent(event: IDomainEvent) {}
+  events: Set<IDomainEvent> = new Set<IDomainEvent>();
+  localMediator = new EventEmitter2();
 
-  registerHandler(event: string, handler: (event: IDomainEvent) => void) {}
+  //vai disparar somente o evento dentro do próprio aggregate
+  applyEvent(event: IDomainEvent) {
+    this.events.add(event);
+    this.localMediator.emit(event.constructor.name, event);
+  }
+
+  registerHandler(event: string, handler: (event: IDomainEvent) => void) {
+    this.localMediator.on(event, handler);
+  }
 }
