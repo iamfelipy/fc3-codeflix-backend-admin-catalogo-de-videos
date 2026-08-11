748355aba630e34dca80d9ba9500d4b0acd9119e
felipy <felipy81br@gmail.com>
2026-08-11 18:10:47 -0300

    feat: Criando repositório do Vídeo

--- src/core/video/infra/db/sequelize/video-model.mapper.ts
+++ src/core/video/infra/db/sequelize/video-model.mapper.ts
@@ -166,7 +166,7 @@ export class VideoModelMapper {
               } as any)
             : null;
         })
-        .filter(Boolean),
+        .filter(Boolean) as ImageMediaModel[],
       audio_video_medias: [trailer, video]
         .map((audio_video_media, index) => {
           return audio_video_media
@@ -183,7 +183,7 @@ export class VideoModelMapper {
               } as any)
             : null;
         })
-        .filter(Boolean),
+        .filter(Boolean) as AudioVideoMediaModel[],
       categories_id: categories_id.map((category_id) =>
         VideoCategoryModel.build({
           video_id: entity.video_id.id,
