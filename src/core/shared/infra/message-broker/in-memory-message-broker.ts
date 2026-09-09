5e73a32bcba3408ba3061fa7e1aeca4d1bd52b67
felipy <felipy81br@gmail.com>
2026-09-09 17:45:39 -0300

    feat: Publicando eventos de upload dos vídeos no RabbitMQ

--- src/core/shared/infra/message-broker/in-memory-message-broker.ts
+++ src/core/shared/infra/message-broker/in-memory-message-broker.ts
@@ -1,11 +1,11 @@
+import { IIntegrationEvent } from '@core/shared/domain/events/domain-event.interface';
 import { IMessageBroker } from '../../application/message-broker.interface';
-import { IDomainEvent } from '../../domain/events/domain-event.interface';
 
 export class InMemoryMessaging implements IMessageBroker {
-  private handlers: { [key: string]: (event: IDomainEvent) => Promise<void> } =
+  private handlers: { [key: string]: (event: IIntegrationEvent) => Promise<void> } =
     {};
 
-  async publishEvent(event: IDomainEvent) {
+  async publishEvent(event: IIntegrationEvent) {
     const handler = this.handlers[event.constructor.name];
     if (handler) {
       await handler(event);
