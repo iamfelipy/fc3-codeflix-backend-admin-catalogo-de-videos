e1db469af4e11a376c4ac38969658e73ef6d310f
felipy <felipy81br@gmail.com>
2026-09-20 19:02:39 -0300

    feat:Corrigindo erro de criação da dead letter queue

--- src/nest-modules/rabbitmq-module/rabbitmq.module.ts
+++ src/nest-modules/rabbitmq-module/rabbitmq.module.ts
@@ -53,6 +53,7 @@ export class RabbitmqModule {
                 name: 'dlx.queue',
                 exchange: 'dlx.exchange',
                 routingKey: '#', //aceito qualquer routing key
+                createQueueIfNotExists: false,
               },
             ],
           }),
